96157523b27e813e6b7a7404a44c13b410529a18
eunsikim <kimeunsik229@gmail.com>
2025-01-14 11:40:34 -0500

    Initial Commit

---
+++ Week-2/HelloBob.py
@@ -0,0 +1,8 @@
+def main():
+    userName = input("What is your name?: ")
+
+    print(f"Hello " + userName)
+
+if __name__ == "__main__":
+	main()
+
---
+++ Week-2/PrintVsPrintLine.py
@@ -0,0 +1,7 @@
+def main():
+    print('Hello World')
+    print("Bob" + " was " + "here.", end="")
+
+if __name__ == "__main__":
+	main()
+
