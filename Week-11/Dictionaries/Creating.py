a751d7c7e63da2eafc4ff48af8ab8b436f66a82d
eunsikim <kimeunsik229@gmail.com>
2025-03-18 09:47:27 -0400

    Week 11 Dictionary example

---
+++ Week-11/Dictionaries/Creating.py
@@ -0,0 +1,24 @@
+def main():
+    # Creates an empty dictionary
+    age = {}
+
+    # Creates a dictionary with some key-value pairs
+    ages = {
+        "Alice": 30,
+        "Bob": 40,
+        "Charlie": 50,
+    }
+
+    # Creates a dictionary using dictionary comprehension
+    numbers = {
+        x: x ** 2 for x in range(10)
+    }
+
+    # Creates a dictionary using the dictionary constructor
+    ages = dict(Alice = 30, Bob = 40, Charlie = 50)
+
+    ages["Daniel"] = 20
+    del ages["Daniel"]
+
+if __name__ == "__main__":
+    main()
