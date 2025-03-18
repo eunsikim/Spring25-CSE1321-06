3fdb083db04db9cb4b749137dccdea22b1d88902
eunsikim <kimeunsik229@gmail.com>
2025-03-18 09:48:10 -0400

    Week 11 Dictionary Example

--- Week-11/Dictionaries/Creating.py
+++ Week-11/Dictionaries/Creating.py
@@ -1,7 +1,7 @@
 def main():
     # Creates an empty dictionary
     age = {}
-
+ 
     # Creates a dictionary with some key-value pairs
     ages = {
         "Alice": 30,
@@ -16,9 +16,6 @@ def main():
 
     # Creates a dictionary using the dictionary constructor
     ages = dict(Alice = 30, Bob = 40, Charlie = 50)
-
-    ages["Daniel"] = 20
-    del ages["Daniel"]
 
 if __name__ == "__main__":
     main()
